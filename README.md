6634f68463534bdf1be1cf344a0b2d7cd25896d4
Ingo Becker <ingo@orgizm.net>
2021-02-11 10:48:56 +0100

    Refactor CLI parser and introduce subcommands

--- README.md
+++ README.md
@@ -10,6 +10,17 @@ This type of sheet is currently the only supported one. It generates 12 kanjis, 
 
 Download the [sample PDF](misc/sheet_123.pdf?raw=true) from the picture above.
 
+To generate those sheets use the `kanjigana` subcommand:
+
+```
+$ shukudai kanjigana --seed 123 --jlpt 2 --output sheet_123.pdf
+# or
+$ shukudai kanjigana -s 123 -j 2 -o sheet_123.pdf
+
+# don't specify a seed to generate a random sheet
+$ shukudai kanjigana -o random.pdf
+```
+
 ## Installation
 
 The easiest way to run `shukudai` is using the container inside this repo. The container contains a propper CKJ-font to render japanese characters as well as the dictionaries required to determine each kanjis meaning and pronounciation.
@@ -20,7 +31,7 @@ Build and run the container as shown below:
 $ git clone https://github.com/ingobecker/shukudai.git
 $ cd shukudai
 $ podman build -t shukudai .
-$ podman container runlabel exec shukudai -- -s 123 -o sheet_123.pdf
+$ podman container runlabel exec shukudai -- kanjigana -s 123 -o sheet_123.pdf
 ```
 
 This will create a file called `sheet_123.pdf` in the current working directory using the seed `123`.
--- lib/shukudai/cli.rb
+++ lib/shukudai/cli.rb
@@ -6,27 +6,71 @@ module Shukudai
 
       options = {
         seed: nil,
-        output: 'sheet.pdf'
+        output: 'sheet.pdf',
+        jtlp: 4
       }
 
-      OptionParser.new do |opts|
-        opts.banner = "Usage: shukudai [options] output.pdf"
+      subcommands_usage = <<~USAGE
+      Subcommands:
+        kanjigana:      train hiragana handwriting and kanji knowledge at once
 
-        opts.on('-s [SEED]', '--seed [SEED]', Integer, 'Reprodruce worksheet with SEED from page (default: random)') do |s|
-          if s < 100 || s > 999
-            abort('make sure seed is between 100 and 999')
+        Use shukudai [subcommand] --help form more options.
+
+      USAGE
+
+      main = OptionParser.new do |opts|
+        opts.banner = 'Usage: shukudai [subcommand]'
+        opts.separator ''
+        opts.separator subcommands_usage
+      end
+
+      subcommands = {
+        kanjigana: OptionParser.new do |opts|
+          opts.banner = 'Usage: shukudai kanjigana [options]'
+
+          opts.on('-s [<number>]',
+                  '--seed [<number>]',
+                  Integer,
+                  'Reprodruce worksheet with <seed> from page (default: random)') do |s|
+
+            options[:seed] = s
           end
-          options[:seed] = s
-        end
 
-        opts.on('-o [OUTPUT]', '--output [OUTPUT]', String, 'Filename of created worksheet (default: sheet.pdf)') do |o|
-          options[:output] = o
-        end
-      end.parse(ARGV)
+          opts.on('-j [<grade>]',
+                 '--jlpt [<grade>]',
+                 Integer,
+                 'Use kanjis of the given JLPT grade. (default: 4)') do |j|
+            options[:jltp] = j
+          end
 
+
+          opts.on('-o [<filename>]',
+                  '--output [<filename>]',
+                  String,
+                  '(default: sheet.pdf)') do |o|
+
+            options[:output] = o
+          end
+        end
+      }
+
+      main.order!
+
+      subcommand = ARGV.shift.to_sym
+      if subcommands.include? subcommand
+        subcommands[subcommand].order!
+        send subcommand, options
+      else
+        abort("#{main.help} Subcommand \"#{subcommand}\" not found. \n\n")
+      end
+
+    end
+
+    def self.kanjigana(opts)
       puts "Loading kanjis..."
-      k = Utils.load_kanji_by_jlpt_grade(path: Config.load[:data][:kanjidic2_xml])
-      s = KanjiHiraganaSheet.new(kanjis: k, seed: options[:seed], output: options[:output])
+      k = Utils.load_kanji_by_jlpt_grade(path: Config.load[:data][:kanjidic2_xml],
+                                         jlpt_grade: opts[:jltp])
+      s = KanjiHiraganaSheet.new(kanjis: k, seed: opts[:seed], output: opts[:output])
       puts "Generating PDF #{s.output} with seed #{s.seed}..."
       s.to_pdf
       puts "Done!"
